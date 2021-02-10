8f18f4a001888d3fcaf9cac982583008a594a124
lostlight <robpirojk@gmail.com>
2021-02-10 19:35:22 +0300

    Task 09.2

--- tasks/task_09/src/main.cpp
+++ tasks/task_09/src/main.cpp
@@ -5,19 +5,24 @@ using namespace std;
 
 int main()
 {
-    vector<int> vec;
     int n;
     cin >> n;
 
-    while (n != 0)
+    if (n == 0)
+        cout << 0;
+    else
     {
-        vec.push_back(n % 2);
-        n /= 2;
-    }
+        vector<int> vec;
+        while (n != 0)
+        {
+            vec.push_back(n % 2);
+            n /= 2;
+        }
 
-    for (size_t i = vec.size() - 1; i > 0; i--)
-        cout << vec[i];
-    cout << vec[0];
+        for (size_t i = vec.size() - 1; i > 0; i--)
+            cout << vec[i];
+        cout << vec[0];
+    }
 
     return 0;
 }
