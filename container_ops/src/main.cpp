6b39dc334bf1eac40edfe4c494b58563e66871af
lostlight <robpirojk@gmail.com>
2021-02-10 10:36:29 +0300

    Container operations

---
+++ container_ops/src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s = "abcdefg";
+    for (char c : s)
+        cout << c << ',';
+    cout << endl;
+
+    vector<int> nums = {1, 5, 2, 3, 4, 5};
+    for (auto c : nums) //Вообще, нужно писать int, но можно заставить компилятор угадывать с помощью auto
+        cout << c << ',';
+    cout << endl;
+
+    int quantity = 0;
+    for (auto c : nums)
+        if (c == 5)
+            quantity++;
+    cout << "There are " << quantity << " fives." << endl;
+
+    quantity = count(begin(nums), end(nums), 5);
+    cout << "There are " << quantity << " fives." << endl;
+
+    sort(begin(nums), end(nums));
+    for (auto c : nums)
+        cout << c << " ";
+
+    return 0;
+}
